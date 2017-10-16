e1767875334786fc8d76cc68a51e8cb9b30d553c
dwormuth <dwormuth@post.harvard.edu>
2017-10-15 23:24:32 -0400

    Fixed error in indexing

---
+++ jupyterworkflow/data.py
@@ -0,0 +1,39 @@
+import os
+from urllib.request import urlretrieve
+
+import pandas as pd
+
+FREMONT_URL = "https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD"
+
+def get_fremont_data(filename='Fremont.csv', url=FREMONT_URL, force_download=False):
+
+    """Download a cache the Fremont Data
+
+    Parameters
+    ----------
+    filename : string (optional)
+        location to save the data
+    url : string (optional)
+        web location of the data
+    force_download : bool (optional)
+        if True, force redownload of data
+
+    Returns
+    -------
+    data : panas.DataFrame
+        The Fremont bridge data
+    """
+    
+    if force_download or not os.path.exists(filename):
+        urlretrieve(URL,filename)
+    data = pd.read_csv(filename, index_col='Date', parse_dates=True)
+    #data = pd.read_csv('Fremont.csv', index_col='Date')
+
+    #try:
+    #    data.index = pd.to_datetime(data.index, format="%m/%d/%Y %H:%M:%S %p")
+    #except TypeError:
+    #    data.index = pd.to_datetime(data.index)
+
+    data.columns = ['West','East']
+    data['Total'] = data['West'] + data['East']
+    return data
